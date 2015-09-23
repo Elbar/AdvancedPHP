be3b6049079ecd0f69b1d64b67e959d4a27973d0
fukuro <e1l2b3a4r5>
2015-09-23 21:39:17 +0600

    Added a test json & function for send messages

---
+++ framework/send.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fukuro
+ * Date: 9/18/15
+ * Time: 12:53 PM
+ */
+    require __DIR__ .'/../PHPMailer/class.phpmailer.php';
+    require_once __DIR__ .'/../PHPMailer/PHPMailerAutoload.php';
+
+    $mail = new PHPMailer();
+
+
+    $mail->isSMTP();
+    $mail->Host    =  "smtp.gmail.com";
+    $mail->SMTPAuth =true;
+    $mail->SMTPSecure = "ssl";
+    $mail->Port  =  465;
+    $mail->CharSet = "UTF-8";
+
+    $body = file_get_contents('test.txt');
+    $mail->Username = $_POST['email'];
+    $mail->Password = 'password';
+    $mail->SetFrom('Username', 'Системный администратор');
+    $mail->Subject   ='Logs of site';
+    $mail->MsgHTML($body);
+    $address = 'Recipient@mail.ru';
+    $mail->AddAddress($address, 'To admin');
+
+    $mail->addAttachment("test.txt");
+
+   if($mail->Send()){
+       echo 'Your message has been successfully sent';
+   }else{
+       echo 'Oops';
+   }
+header('Refresh: 2; url=index.php');
