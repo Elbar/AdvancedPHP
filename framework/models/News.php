e98a66b24ba68832a463d913056b5d18700be6cb
fukuro <e1l2b3a4r5>
2015-09-23 21:52:47 +0600

    News model

---
+++ framework/models/News.php
@@ -0,0 +1,19 @@
+<?php
+
+
+//namespace Application\Models;
+
+/**
+ * Class News
+ * @property $id
+ * @property $title
+ * @property $text
+ * */
+
+class News
+extends \AbstractModel
+{
+    protected static $table = 'news';
+}
+
+
