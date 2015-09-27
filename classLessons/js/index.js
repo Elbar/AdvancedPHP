73e7f1f1b445262b86201fc26a215eac28415f88
fukuro <e1l2b3a4r5>
2015-09-27 21:49:19 +0600

    classLessons

---
+++ classLessons/js/index.js
@@ -0,0 +1,77 @@
+/**
+ * Created by fukuro on 9/26/15.
+ */
+console.log('Я начал выполняться');
+/**
+ * addEventListener
+
+window.addEventListener('load', function(){
+    var c1 =document.querySelector('.c1');
+    var c2 = document.querySelector('.c2');
+    var c3 = document.querySelector('.c3');
+
+
+    c1.addEventListener('click',function() {
+        console.log('1');
+    });
+        c1.addEventListener('click',function() {
+        console.log('2');}, true);
+
+    c2.addEventListener('click',function() {
+       console.log('3');
+    });
+        c2.addEventListener('click',function(e) {
+
+        console.log('4');
+    e.stopPropagation();  },true);
+
+    c3.addEventListener('click',function() {
+        console.log('5');
+    });
+        c3.addEventListener('click',function() {
+        console.log('6');},true);
+});
+
+ */
+    /**
+     * Решения через document.querySelector('.smth')
+     */
+    window.addEventListener('load',function(){
+    document.querySelector('.c3').addEventListener('click',function(){
+      //event.target
+        //остановка сплытия
+
+       // event.stopPropagation();
+        document.querySelector('.c3').style.backgroundColor = 'white';
+    });
+
+
+
+  document.querySelector('.c2').addEventListener('click',function(){
+    document.querySelector('.c2').style.backgroundColor = 'white';
+});
+
+
+    document.querySelector('.c1').addEventListener('click',function(){
+        document.querySelector('.c1').style.backgroundColor = 'lightblue';
+    });
+});
+
+
+
+/**
+ *
+
+window.addEventListener('load', function() {
+    console.log("Я сделал");
+
+    document.querySelector('h2').innerHTML = 'I changed HTML';
+    //MDN Moziilla Developers Network
+    document.querySelector('h1').addEventListener('click', function(){
+        alert("По мне кликнули!");
+    });
+});
+*/
+
+console.log(' Я закончил выполняться');
+
---
+++ classLessons/js/lib.js
@@ -0,0 +1,47 @@
+/**
+ * Created by fukuro on 9/26/15.
+ */
+    /*
+var age = prompt("Your age", 18);
+
+
+    var checkAccess =(age >= 18)?
+        function () {
+        return true;
+    }: function() {
+        checkAccess = function () {
+            return confirm("forbidden");
+        };
+
+    }
+alert(checkAccess() ? "OK" : "forbidden");
+
+
+        */
+
+/**
+ * Function Expression
+ */
+
+/**
+ *
+
+(function() {
+    var ecmaSupport = 1;
+
+}());
+ */
+
+
+/**
+ * Factorial function
+ * @param n
+ * @returns {number}
+
+function f(n) {
+    return n > 1 ? n * f(n - 1) : 1;
+}
+f(2);
+
+ */
+
